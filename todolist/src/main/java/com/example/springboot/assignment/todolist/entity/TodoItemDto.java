a58ed8e3a179664a94efd2a2f13444481c0b666a
Sahithi <sahithi.maligireddy@zemosolabs.com>
2022-06-15 19:58:34 +0530

    commit3

--- todolist/src/main/java/com/example/springboot/assignment/todolist/entity/TodoItemDto.java
+++ todolist/src/main/java/com/example/springboot/assignment/todolist/entity/TodoItemDto.java
@@ -6,43 +6,4 @@ public class TodoItemDto {
     private String title;
     private boolean done;
 
-    /*public TodoItemDto(){}
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
-    }
-
-    @Override
-    public String toString() {
-        return "TodoItemDto{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", done=" + done +
-                '}';
-    }
-
-    public TodoItemDto(int id, String title, boolean done) {
-        this.id = id;
-        this.title = title;
-        this.done = done;
-    }*/
 }
